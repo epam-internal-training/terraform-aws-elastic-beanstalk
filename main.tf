4115fca28dcaaf137b3004511924838cfc120093
Srinivasulu Pamulapati <Srinivasulu.Pamulapa@cantire.com>
2023-07-19 10:26:45 +0530

    update module

--- main.tf
+++ main.tf
@@ -1,5 +1,52 @@
+data "aws_iam_policy_document" "ec2" {
+  statement {
+    sid = ""
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+
+  statement {
+    sid = ""
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ssm.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "ec2" {
+
+  name               = "iam-role-eb-ec2"
+  assume_role_policy = join("", data.aws_iam_policy_document.ec2[*].json)
+}
+
+
+
+resource "aws_iam_instance_profile" "ec2" {
+
+  name = "iam-instance-profile-eb-ec2"
+  role = join("", aws_iam_role.ec2[*].name)
+}
+
+
 resource "aws_elastic_beanstalk_application" "main" {
-  name        = var.application_name
+  name = var.application_name
   tags = var.tags
 }
 
@@ -15,8 +62,16 @@ resource "aws_elastic_beanstalk_environment" "main" {
     for_each = var.settings
     content {
       namespace = setting.value["namespace"]
-      name = setting.value["name"]
-      value = setting.value["value"]
+      name      = setting.value["name"]
+      value     = setting.value["value"]
     }
   }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = join("", aws_iam_instance_profile.ec2[*].name)
+    resource  = ""
+  }
+
 }
